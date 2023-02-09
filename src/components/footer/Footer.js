1eb868a6007b0c9f638a32d0e12c43d0cb873d7c
Mayur Hadole <mhadole@tucows.com>
2023-02-09 15:23:11 -0500

    update year in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji(greeting.username + " © 2022")}
+          {emoji(greeting.username + " © 2023")}
         </p>
         {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
